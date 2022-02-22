af53ec9570242bb9e2b549328a93d7f2269c6e16
David Artaza <daveartdev@gmail.com>
2022-02-22 13:24:28 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// my human age (constant)
+const myAge = 36;
+
+//early development years (varible(let))
+let earlyYears = 2;
+
+//multiplier and reassignment of variable value
+earlyYears *= 10.5;
+
+//subtraction of variable
+let laterYears= myAge - 2;
+
+//multiplication of variable
+laterYears *= 4;
+
+//check
+console.log(`${earlyYears}  ${laterYears}`)
+
+//adding of 2 variables
+let myAgeInDogYears = earlyYears + laterYears;
+
+//string method
+let myName = "David".toLowerCase();
+
+// output
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
